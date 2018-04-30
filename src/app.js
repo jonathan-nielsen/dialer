a0610179f799c76993ed0ca94f38d693e08f93ff
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2018-04-30 23:45:13 +0200

    Fixes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import debug from 'debug';
 const appLog = debug('app:config');
 const DBLog = debug('app:db');
 
-// Packages
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -12,7 +11,6 @@ import mongoose from 'mongoose';
 import session from 'express-session';
 appLog('Packages loaded.');
 
-// Routes
 import * as routes from './routes';
 
 const app = express();
@@ -33,12 +31,10 @@ app.use(flash());
 appLog('Packages enabled.');
 
 app.on('db-connected', function() {
-	// TODO: Readdir recursively, add folder structure as url.
 	for (const key in routes) {
 		app.use('', routes[key]);
 	}
 
-	// 404
 	app.use(function(req, res) {
 		res.send('<h1>404 - Not found</h1><a href="/">Home</a><br><a href="#" onclick="window.history.back()">Back</a>');
 	});
